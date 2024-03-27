3e8b988f13b134282cb4fcea0499a3f167cd7d99
vedangi patel <vepatel@netclues.lan>
2024-03-27 11:19:40 +0530

    Fetched All Data with async Await

--- employee_controller.js
+++ employee_controller.js
@@ -1,20 +1,40 @@
 const db = require('./db_config');
 const Employee = db.employees;
 
-function findAllEmployees(req, res) {
-    Employee.findAll().then((data) => {
-        req.send(data);
-        // console.log(json(data));
-    }).catch(err => {
-        res.send(err);
-    });
+function createEmployee(req, res) {
+    // try {
+    //     if (!req.body.name || !req.body.email || !req.body.gender || !req.body.experience) {
+    //         return res.status(400).send("Bad Request");
+    //     }
+
+    //     const empployeeObject = {
+    //         name : req.body.name,
+    //         email: req.body.email,
+    //         gender: req.body.gender,
+    //         experiance: req.body.experiance
+    //     }
+    //     // Logic to create employee
+    // } catch (err) {
+    //     res.status(500).send(err.message);
+    // }
+
+    // Employee.create(Employee).then((data) => {
+    //     res.send(data);
+    // }).catch( (err) => {
+    //     throw new Error (err.message);
+    // });
 }
 
-function createEmplolyee(req, res) {
-
+async function findAllEmployees() {
+    try {
+        const data = await Employee.findAll();
+        return data;
+    } catch (err) {
+        throw new Error(err.message);
+    }
 }
 
 module.exports = {
-    findAllEmployees,
-    createEmplolyee
+    createEmployee,
+    findAllEmployees
 };
--- server.js
+++ server.js
@@ -1,3 +1,25 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const controller = require('./employee_controller');
+
+// app.use(bodyParser.json());
+
+// const db = require('./db_config');
+
+// //create table if not exist
+// db.sequelize.sync();
+
+// app.get('/', (req, res) => {
+//     res.send("Home Page !");
+// });
+
+// app.get('/AllEmployees', async (req, res) => {
+//     controller.findAllEmployees(req, res);
+// });
+
+// app.listen(3000);
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,15 +29,26 @@ app.use(bodyParser.json());
 
 const db = require('./db_config');
 
-//create table if not exist
+// Create table if not exist
 db.sequelize.sync();
 
 app.get('/', (req, res) => {
     res.send("Home Page !");
 });
 
-app.get('/AllEmployees', (req, res) => {
-    controller.findAllEmployees(req, res);
+// app.post('/createEmployee', async (req, res) => {
+//     controller.createEmployee(req, res);
+// })
+
+app.get('/AllEmployees', async (req, res) => {
+    try {
+        const employees = await controller.findAllEmployees();
+        res.json(employees);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
